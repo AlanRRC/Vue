3758518a54a3447bc75d29fdecbe33388892bb18
Alan Simpson <asimpson@rrc.ca>
2020-12-08 19:28:30 -0600

    Computed parameters

--- app.js
+++ app.js
@@ -5,32 +5,25 @@ const app = Vue.createApp({
             url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author:'patrick rothfuss'},
-                { title: 'the way of kings', author:'brandon sanderson'},
-                { title: 'the final empire', author:'brandon sanderson'},
+                { title: 'name of the wind', author:'patrick rothfuss', img:'assets/1.jpg', isFav:true},
+                { title: 'the way of kings', author:'brandon sanderson', img:'assets/2.jpg', isFav:false},
+                { title: 'the final empire', author:'brandon sanderson', img:'assets/3.jpg', isFav:true},
             ]
         }
     },
     methods:{
-        // changeTitle(title){
-        //     // this refers to app
-        //     this.title = title
-        // }
+
         toggleShowBooks(){
             this.showBooks = !this.showBooks
         },
-
-        // Mouse event methods
-        // handleEvent(e, data){
-        //     console.log(e, e.type)
-        //     if(data){
-        //         console.log(data)
-        //     }
-        // },
-        // handleMousemove(e){
-        //     this.x = e.offsetX
-        //     this.y = e.offsetY
-        // }
+        toggleFav(book){
+            book.isFav = !book.isFav
+        },
+    },
+    computed:{
+        filteredBooks(){
+            return this.books.filter((book) => book.isFav)
+        }
     }
 })
 
