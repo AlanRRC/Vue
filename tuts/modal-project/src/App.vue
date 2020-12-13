347bbdc9070668aaa00e66e1acc06da2aa059bfa
Alan Simpson <asimpson@rrc.ca>
2020-12-12 20:33:17 -0600

    New component

--- tuts/modal-project/src/App.vue
+++ tuts/modal-project/src/App.vue
@@ -1,16 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data(){
+    return {
+      title: 'My first vue app :) '
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
+
 }
 </script>
 
@@ -22,5 +33,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1{
+  border-bottom: 1px solid #DDD;
+  /* display: inline-block; */
+  padding-bottom: 10px;
 }
 </style>
